c0b5f37e8828d43c198704a03674ca10d5836ee9
YeyaSwizaw <samuel.sleight@gmail.com>
2014-04-02 23:11:20 +0100

    Added comments

--- Search.ml
+++ Search.ml
@@ -1,11 +1,17 @@
+(* Takes an option type and returns either None or Some (result of function 
+ * applied to value) *)
 let mapmaybe f x = match x with 
     | None -> None
     | Some x' -> f x';;
 
+(* Calls a function for each item in a list *)
 let rec foreach f ls = match ls with
     | [] -> ()
     | x::ls' -> f x; foreach f ls';;
 
+(* Performs search from the start node to the end node, using the provided frontier.
+ * The frontier implementation decides the search strategy, for example a queue 
+ * is Breadth First Search. *)
 let search frontier start goal =
     let rec mem set x = match set with
         | [] -> false
--- Test.ml
+++ Test.ml
@@ -1,16 +1,21 @@
 open Search;;
 
+(* A breadth first search frontier - uses a queue *)
 class ['a] bf = 
     object (self)
         val mutable ls = []
 
+        (* Appends an item to the queue *)
         method push (x : 'a) = ls <- (ls @ [x])
 
+        (* Returns Some (top of queue) unless the queue is empty, in which case
+         * returns None. *)
         method pop : 'a option = match ls with
             | [] -> None
             | x::ls' -> ls <- ls'; Some x
     end;;
 
+(* A very basic 'grid' location. Basically just an (x, y) coordinate *)
 class gridstate xinit yinit =
     object (self)
         val x : int = xinit
@@ -23,6 +28,7 @@ class gridstate xinit yinit =
             (x == other#getx) && (y == other#gety)
     end;;
 
+(* A search node storing a grid state *)
 class gridnode st pa ac =
     object (self)
         val s : gridstate = st
@@ -37,15 +43,19 @@ class gridnode st pa ac =
             | None -> Printf.printf "(%d, %d)" s#getx s#gety
             | Some (xd, yd) -> Printf.printf "(%d, %d) -> (%d, %d)" xd yd s#getx s#gety
 
+        (* Returns true if the stored state is the same as the provided node's state *)
         method equals (other : gridnode) =
             s#equals other#state
 
+        (* Creates a new node identical to this one *)
         method copy =
             new gridnode s p a
 
+        (* Creates a new node - a copy of this one with the provided move applied *)
         method applymove (xd, yd) =
             new gridnode (new gridstate (s#getx + xd) (s#gety + yd)) (Some self#copy) (Some (xd, yd))
 
+        (* Returns a list of all possible states after this one *)
         method expand = 
             Printf.printf "Expanding Node (%d, %d)" s#getx s#gety; print_newline ();
             [self#applymove (1, 0);
@@ -54,5 +64,4 @@ class gridnode st pa ac =
              self#applymove (0, -1)]
     end;;
 
-(*let bfs = search (new bf);;*)
 let createnode x y = new gridnode (new gridstate x y) None None;;
